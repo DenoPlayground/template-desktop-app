b55aed9fb8b9432f6d3066d32d90c68bc5ce244d
Mqxx <62719703+Mqxx@users.noreply.github.com>
2026-07-09 16:10:15 +0200

    init

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,5 @@
-
-export default (signal: AbortSignal) : void => {
-  Deno.serve({port: 8080, signal}, () => {
-    return new Response("Hello from Deno server!");
-  });
-};
+Deno.readTextFile('./dist/bundle/index.html').then(
+  (file) => Deno.serve({port: 8080}, () => {
+    return new Response(file);
+  })
+);
